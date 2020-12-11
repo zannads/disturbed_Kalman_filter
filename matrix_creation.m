5d0e611a9c45ef042f87866ad1bbac04fbc9a4ae
Dennis <65568528+zannads@users.noreply.github.com>
2020-12-11 13:36:07 +0100

    changed the way we build C_tau and Dw_tau

--- matrix_creation.m
+++ matrix_creation.m
@@ -11,7 +11,7 @@ A = [0.3, 0.4; 0, 0.2];
 % Bu n x m
 Bu = [1; 7];
 % Bw n x m_w
-Bw = [0.001, 0; 0.001, 0];        
+Bw = [0.001, 0; 0.002, 0];        
 % Du m x m_n
 Du = 0.3;
 
@@ -19,7 +19,7 @@ Du = 0.3;
 % C p x n 
 C = [1, 1]; 
 % Dw p x m_w
-Dw = [0, 0.3];
+Dw = [0, 0.4];
 D = 0;
 
 tau = 3;            %delay steps
@@ -35,7 +35,7 @@ if( (length(A) - rank(R_m)) == 0)
 end
 
 % 
-% syms theta
+% 
 % condi = [ A-eye(size(A))*exp(i*theta), Bw;
 %   C, Dw]
 % test = rank(condi);
@@ -43,7 +43,7 @@ end
 %enlarge 
 [~, m_n] = size(Du);
 [p, ~] = size(C);
-[A_tau, Bw_tau, Bu_tau, C_tau, Dw_tau, D_tau] = enlarge(A, [-Bu*Du,  Bw], Bu, C, [zeros(p, m_n), Dw], D, tau);
+[A_tau, Bw_tau, Bu_tau, C_tau, Dw_tau, D_tau] = enlarge(A, Bw, Bu, C, Dw, Du, D, tau);
 
 %check conditions for enlarged system
 %our_condition = Bu*Du
@@ -55,14 +55,17 @@ R_m_tau = ctrb(A_tau, Bw_tau);
 if( length(A_tau) - rank(R_m_tau) == 0 )
     disp('final system reachable');
 end
-% condi = [ A_tau-eye(size(A_tau))*exp(i*theta), Bw_tau;
-%   C_tau, Dw_tau];
-% test = rank(condi);
+
+syms theta
+condi = [ A_tau-eye(size(A_tau))*exp(i*theta), Bw_tau;
+  C_tau, Dw_tau];
+
 
 %compute kalman
-Q_tilde = Bw_tau*Bw_tau';
-R_tilde = Dw_tau*Dw_tau';
-
+variance_mat = [Bw_tau; Dw_tau]*[Bw_tau; Dw_tau]';
+Q_tilde = Bw_tau(:, 2:3)*Bw_tau(:, 2:3)';
+R_tilde = Dw_tau(:, 2:3)*Dw_tau(:, 2:3)';
+%%
 [Y,K,L]=idare(A_tau',C_tau',Q_tilde,R_tilde);
 K = -K'
 Y
@@ -72,35 +75,36 @@ L
     
 %% simulink preparation
 % the real system evolves without -Bu*Du ...
-[A_simu, Bw_simu, Bu_simu, C_simu, Dw_simu, Du_simu] = enlarge(A, Bw, Bu, C, Dw, D, tau);
-B_simu = [Bu_simu, Bw_simu];
-D_simu = [Du_simu, Dw_simu];
-[n_en, ~] = size(A_simu);
-[~, m_en] =size(B_simu);
-C_simu_state = eye( n_en, n_en);
-D_simu_state = zeros(n_en, m_en);
-
-A_KF = A_tau + K*C_tau;
-B_KF = [Bu_tau, -K];
-[n_kf, ~] = size(A_KF);
-[~, m_kf] = size(B_KF);
-C_KF = eye( n_kf, n_kf );
-D_KF = zeros(n_kf, m_kf);
-
-[n_kf, ~] = size(A);
-A_KF_red = A + K(1:n_kf)*C;
-B_KF_red = [Bu, -K(1:n_kf)];
-[~, m_kf] = size(B_KF_red);
-C_KF_red = eye( n_kf, n_kf);
-D_KF_red = zeros(n_kf, m_kf);
+n = size(A, 1);
+p = size(C, 1);
+m = size(Bu, 2);
+B_simu = [Bu, Bw];
+D_simu = [zeros(p,m), Dw];
+C_simu_state = eye( n, n);
+m_en =size(B_simu, 2);
+D_simu_state = zeros(n, m_en);
+% 
+% A_KF = A_tau + K*C_tau;
+% B_KF = [Bu_tau, -K];
+% [n_kf, ~] = size(A_KF);
+% [~, m_kf] = size(B_KF);
+% C_KF = eye( n_kf, n_kf );
+% D_KF = zeros(n_kf, m_kf);
+% 
+% [n_kf, ~] = size(A);
+% A_KF_red = A + K(1:n_kf)*C;
+% B_KF_red = [Bu, -K(1:n_kf)];
+% [~, m_kf] = size(B_KF_red);
+% C_KF_red = eye( n_kf, n_kf);
+% D_KF_red = zeros(n_kf, m_kf);
 
 
 sim('disturbed_kalman');
 %% functions
-function [A_tau, Bw_tau, Bu_tau, C_tau, Dw_tau, D_tau] = enlarge(A, Bw, Bu, C, Dw, D, tau)
+function [A_tau, Bw_tau, Bu_tau, C_tau, Dw_tau, D_tau] = enlarge(A, Bw, Bu, C, Dw, Du, D, tau)
 %get dimensions 
 [n, m, p, m_w ] = check_dimensions(A, Bw, Bu, C, Dw, D);
-
+m_n = size(Du, 2);
 if( n ~=0 ) %if everything is ok
     
     A_tau = [A, Bu, zeros(n, tau-1);
@@ -108,11 +112,14 @@ if( n ~=0 ) %if everything is ok
         zeros(1, n+tau)];
     Bu_tau = [zeros(n+tau-1, m);
         eye(m)];
-    Bw_tau = [ Bw;
-        zeros(tau, m_w)];
+    Bw_tau = [zeros(n+(tau-1)*m, m_n), [Bw; zeros((tau-1)*m, m_w)];
+              -Du, zeros(m, m_w)];
     
-    C_tau = [C, zeros(p, tau)];
-    Dw_tau = Dw;
+    C_tau = [C, zeros(p, tau*m);  
+             zeros(tau*m,n), eye(tau*m)];
+    
+    Dw_tau = [zeros(p, m_n),        Dw;
+              ones(tau, m_n)*Du,    zeros(tau, m_w)];
     D_tau = D;
 end
 end
